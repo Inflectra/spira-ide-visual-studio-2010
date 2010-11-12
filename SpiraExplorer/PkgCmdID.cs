2038ff34637277a421db1d43098f61cf4cf77062
mmorrey <mmorrey@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-11-12 15:25:06 +0000

    Initial checkin of VS2010 Addon.
    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@13037 5d4b435c-4d46-d949-aab2-e601ca53af14

---
+++ SpiraExplorer/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Inflectra.SpiraExplorer
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdViewToolWindow =        0x100;
+        public const uint cmdViewExplorerWindow =    0x101;
+
+    };
+}
---
+++ SpiraExplorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpiraExplorer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Inflectra")]
+[assembly: AssemblyProduct("SpiraExplorer")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("SpiraExplorer_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e9f22c38e5d94bdf2be6558a7bcad779d398120f8fa1390523c49945b17c675ac977ffbea039145ab2b103d0658bd0c0a1d888a33532299e8e2b3c1ed01c8359f1ab8a2e105c4acc095c51803215ea07e1c877e7cbe6899d28b4e2f188198214bf63e1ea7fee2485dd2fd4e56deda59d8af515b542727b4f235f5a0edeb69c88")]
+
