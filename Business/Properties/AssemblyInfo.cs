8ca4cc2e0f409e8f2650a2739b682008eaa12e92
mmorrey <mmorrey@5d4b435c-4d46-d949-aab2-e601ca53af14>
2013-04-24 20:16:20 +0000

    More code checkins.
    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@21641 5d4b435c-4d46-d949-aab2-e601ca53af14

--- Business/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SpiraExplorer.Business")]
-[assembly: AssemblyDescription("Helper functions for SpiraExplorer")]
-[assembly: ComVisible(false)]
-[assembly: Guid("510cbc66-5b07-4d22-99a0-f28709d9453d")]
-[assembly: AssemblyVersion("3.2.1.14503")]
-[assembly: AssemblyFileVersion("3.2.1.14503")]
--- SpiraExplorer.resources/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCulture("en-US")]
-[assembly: AssemblyTitle("SpiraExplorerBusiness.Resources")]
-[assembly: AssemblyDescription("Resources file for the SpiraTeam Explorer Add-In for VS2010")]
-[assembly: AssemblyVersion("3.2.1.14503")]
-[assembly: AssemblyFileVersion("3.2.1.14503")]
